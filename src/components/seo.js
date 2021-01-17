035f06d340fc9bb2eead114e01f642c4d4622b26
Easaaa <easaaa@protonmail.com>
2021-01-17 19:17:52 +0100

    Fix twitter description

--- src/components/seo.js
+++ src/components/seo.js
@@ -5,10 +5,10 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from "react"
-import PropTypes from "prop-types"
-import Helmet from "react-helmet"
-import { useStaticQuery, graphql } from "gatsby"
+import React from 'react';
+import PropTypes from 'prop-types';
+import Helmet from 'react-helmet';
+import { useStaticQuery, graphql } from 'gatsby';
 
 const SEO = ({ description, lang, meta, title }) => {
   const { site } = useStaticQuery(
@@ -18,14 +18,18 @@ const SEO = ({ description, lang, meta, title }) => {
           siteMetadata {
             title
             description
-            social { twitter }
+            social {
+              twitter
+            }
           }
         }
       }
     `
-  )
+  );
 
-  const metaDescription = description || site.siteMetadata.description
+  const metaDescription = description || site.siteMetadata.description;
+
+  const summary = `Hi there! This is Leonardo. I’m a passionate and avid learner and I love to experiment new things. I am a self-taught programmer and, in this blog, I’d like to share all my findings, difficulties and achievements while learning everything I can about blockchain technology.`;
 
   return (
     <Helmet
@@ -49,11 +53,11 @@ const SEO = ({ description, lang, meta, title }) => {
         },
         {
           property: `og:type`,
-          content: `website`,
+          content: `https://www.leonardotononi.com/`,
         },
         {
           name: `twitter:card`,
-          content: `summary`,
+          content: summary,
         },
         {
           name: `twitter:creator`,
@@ -69,20 +73,20 @@ const SEO = ({ description, lang, meta, title }) => {
         },
       ].concat(meta)}
     />
-  )
-}
+  );
+};
 
 SEO.defaultProps = {
   lang: `en`,
   meta: [],
   description: ``,
-}
+};
 
 SEO.propTypes = {
   description: PropTypes.string,
   lang: PropTypes.string,
   meta: PropTypes.arrayOf(PropTypes.object),
   title: PropTypes.string.isRequired,
-}
+};
 
-export default SEO
+export default SEO;
