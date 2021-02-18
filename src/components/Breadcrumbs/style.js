2c2a591b097898bea6277eb74870a42f27b6cf19
Leonardo T <easaaa.dev@gmail.com>
2021-02-18 10:42:22 +0100

    Trivial commit

---
+++ src/components/Breadcrumbs/style.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+import { devices, fonts } from '../../constants';
+import Img from 'gatsby-image';
+import { Link } from 'gatsby';
+
+export const Wrapper = styled.div``;
+
+export const LinkItem = styled(Link)``;
